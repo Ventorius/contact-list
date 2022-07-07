08a50aeab4b9f139ff1dbad7b9481eda17bfaeb3
Sebastian Basznianin <avaven@gmail.com>
2022-07-08 00:50:03 +0200

    feat: add eslint & prettier, reformat code

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,27 @@
-import React from "react";
-import apiData from "./api";
-import PersonInfo from "./PersonInfo";
-import { useQuery} from "react-query";
-
-
+import React from 'react';
+import apiData from './api';
+import PersonInfo from './PersonInfo';
+import { useQuery } from 'react-query';
 
 function App() {
   const [data, setData] = React.useState([]);
   const [selected, setSelected] = React.useState([]);
 
-  const query = useQuery('contacts', apiData)
+  const query = useQuery('contacts', apiData);
 
-  console.log(query)
+  console.log(query);
 
   //  TODO fetch contacts using apiData function, handle loading and error states
 
   return (
-
     <div className="App">
       <div className="selected">Selected contacts: {selected.length}</div>
       <div className="list">
         {data.map((personInfo) => (
-          // @ts-ignore
           <PersonInfo key={personInfo.id} data={personInfo} />
         ))}
       </div>
     </div>
-
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import {QueryClient, QueryClientProvider} from "react-query";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import './index.css';
 
-const queryClient = new QueryClient()
-
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-    <App />
-      </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
