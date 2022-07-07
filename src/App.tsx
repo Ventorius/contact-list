63e84fcd0baa26125185fd024845c7ca77840efd
Sebastian Basznianin <avaven@gmail.com>
2022-07-08 00:41:32 +0200

    feat: add react-query, make basic call for data

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import apiData from "./api";
 import PersonInfo from "./PersonInfo";
+import { useQuery} from "react-query";
+
+
 
 function App() {
   const [data, setData] = React.useState([]);
   const [selected, setSelected] = React.useState([]);
 
+  const query = useQuery('contacts', apiData)
+
+  console.log(query)
+
   //  TODO fetch contacts using apiData function, handle loading and error states
 
   return (
+
     <div className="App">
       <div className="selected">Selected contacts: {selected.length}</div>
       <div className="list">
@@ -18,6 +26,7 @@ function App() {
         ))}
       </div>
     </div>
+
   );
 }
 
