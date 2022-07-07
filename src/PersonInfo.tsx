0e13645771f54bc49ffb00fb1339cec6aa290d0d
Sebastian Basznianin <avaven@gmail.com>
2022-07-08 01:48:38 +0200

    fix: move styles to separate file

--- src/PersonInfo.tsx
+++ src/PersonInfo.tsx
@@ -10,17 +10,6 @@ function PersonInfo({ data, onSelect, isSelected }: Props) {
   return (
     <div
       onClick={() => onSelect(data.id)}
-      style={{
-        display: 'flex',
-        height: '100px',
-        justifyContent: 'center',
-        flexDirection: 'column',
-        padding: '32px',
-        boxShadow: '0px 1px 2px 0px rgba(0, 0, 0, 0.15)',
-        margin: '10px 0',
-        background: '#fff',
-        cursor: 'pointer',
-      }}
       className={isSelected ? 'person-info-selected' : 'person-info'}>
       <div className="firstNameLastName">{data.firstNameLastName}</div>
       <div className="jobTitle">{data.jobTitle}</div>
